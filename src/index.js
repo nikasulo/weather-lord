9c74053d7f193b6efbf082924daf301dcbc459a0
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-04 11:50:05 +0100

    Write method to fetch data

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { renderData, capitalizeString,renderCountryName } from './auxiliaries.js';
+
+import Styles from './stylesheets/main.scss';
+
+const BASE_URL = "https://api.aerisapi.com/";
+const CLIENT_ID = "sE56PqAn4S3wCf9HQw8n1";
+const APP_SECRET = "eMGjiglIxVapPzQCk1dVFlCGFtvdJuq63gYPLhUm";
+const SECRETS = `client_id=${CLIENT_ID}&client_secret=${APP_SECRET}`;
+
+const getData = (endpoint, action, selector, extraQuery = '') => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    const url = BASE_URL + `${endpoint.toLowerCase()}/` + 
+                            `${action.toLowerCase()}${selector}${extraQuery.toLowerCase()}` + SECRETS;
+
+    request.open('GET', url, true);
+    request.onload = () => {
+      if (request.status == 200){
+        resolve(JSON.parse(request.response));
+      } else {
+        reject(request.statusText);
+      }
+    };
+
+    request.onerror = () => {
+      reject(request.statusText);
+    };
+
+    request.send();
+  });
+}
