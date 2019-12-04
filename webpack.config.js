c719f3151ca3e490bb4414250bb25f4b7e4b0b54
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-04 11:51:41 +0100

    Setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
-  plugin: [
-    {
-      test: /\.(s)*css/,
-      use: ['style-loader','css-loader','sass-loader']
-    }
+
+  plugins: [
+    new HTMLWebpackPlugin({template: './dist/index.html'}),
+    new CleanWebpackPlugin({template: './dist/index.html'}),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(s)*css/,
+        use: ['style-loader','css-loader','sass-loader']
+      }
+    ],
+  },
+
   devtool: 'inline-source-map',
+
   output: {
-    path: path.resolve(__dirname, './dist/index.html'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   }
 }
